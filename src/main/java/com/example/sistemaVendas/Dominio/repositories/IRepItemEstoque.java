d2948fbd8b89fbb7d41b3468a41a9f2564587024
Maria Contu <mariaescontu@gmail.com>
2023-11-10 21:09:09 +0000

    att

--- src/main/java/com/example/sistemaVendas/Dominio/repositories/IRepItemEstoque.java
+++ src/main/java/com/example/sistemaVendas/Dominio/repositories/IRepItemEstoque.java
@@ -1,5 +1,19 @@
-package com.example.sistemaVendas.Dominio;
+package com.example.sistemaVendas.Dominio.repositories;
+
+import java.util.List;
+
+import com.example.sistemaVendas.Dominio.model.ItemEstoque;
 
 public interface IRepItemEstoque {
     
+    List<ItemEstoque> listarItensEstoque();
+
+    ItemEstoque obterItemEstoquePorId(long itemId);
+
+    void adicionarItemEstoque(ItemEstoque itemEstoque);
+
+    void atualizarItemEstoque(ItemEstoque itemEstoque);
+
+    void removerItemEstoque(long itemId);
+
 }
--- src/main/java/com/example/sistemaVendas/Dominio/services/ServicoVendas.java
+++
@@ -1,7 +0,0 @@
-package com.example.sistemaVendas.Dominio.services;
-
-import com.example.sistemaVendas.Dominio.repositories.IRepOrcamentos;
-
-public class ServicoVendas implements IRepOrcamentos{
-    
-}
