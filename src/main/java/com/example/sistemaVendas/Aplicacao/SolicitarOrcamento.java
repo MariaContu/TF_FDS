ac73a6d92685e52987ccd31311e1b60f0ca0ee9c
Maria Contu <mariaescontu@gmail.com>
2023-11-16 21:50:03 +0000

    init solicitarOrcamento

--- src/main/java/com/example/sistemaVendas/Aplicacao/SolicitarOrcamento.java
+++ src/main/java/com/example/sistemaVendas/Aplicacao/SolicitarOrcamento.java
@@ -1,5 +1,20 @@
 package com.example.sistemaVendas.Aplicacao;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.sistemaVendas.Dominio.model.Orcamento;
+import com.example.sistemaVendas.Dominio.model.Pedido;
+import com.example.sistemaVendas.Dominio.services.ServicoVendas;
+
+@Component
 public class SolicitarOrcamento {
-    
+    @Autowired
+    private ServicoVendas servicoVendas;
+
+    public void solicitarOrcamento(int id, Date data, String nomeCliente, Pedido pedido)    {
+        servicoVendas.addOrcamento(new Orcamento(id,data,nomeCliente,pedido));
+    }
 }
