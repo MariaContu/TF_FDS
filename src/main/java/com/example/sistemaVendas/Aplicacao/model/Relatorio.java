4b011a1745becc4c1d6603f6e68296ff9d296ff5
Maria Contu <mariaescontu@gmail.com>
2023-11-16 21:28:58 +0000

    reorganizing

---
+++ src/main/java/com/example/sistemaVendas/Aplicacao/model/Relatorio.java
@@ -0,0 +1,5 @@
+package com.example.sistemaVendas.Aplicacao.model;
+
+public class Relatorio {
+    
+}
---
+++ src/main/java/com/example/sistemaVendas/Aplicacao/services/ServicoRelatorio.java
@@ -0,0 +1,5 @@
+package com.example.sistemaVendas.Aplicacao.services;
+
+public class ServicoRelatorio {
+    
+}
