b6cf1d5cfdebdbff158dc7b1e70dbdb2d082d709
Maria Contu <mariaescontu@gmail.com>
2023-11-17 23:57:07 +0000

    prepare tests

--- src/test/java/com/example/sistemaVendas/SistemaVendasApplicationTests.java
+++ src/test/java/com/example/sistemaVendas/SistemaVendasApplicationTests.java
@@ -1,10 +1,22 @@
 package com.example.sistemaVendas;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.sistemaVendas.Dominio.repositories.IRepProdutos;
+import com.example.sistemaVendas.Dominio.services.ServicoVendas;
 
 @SpringBootTest
 class SistemaVendasApplicationTests {
+
+	@Mock
+    private IRepProdutos repProdutos;
+
+    @InjectMocks
+    private ServicoVendas servicoVendas;
+
 
 	@Test
 	void contextLoads() {
@@ -12,6 +24,16 @@ class SistemaVendasApplicationTests {
 
 	@Test
 	void testaCustoDoPedido()	{
+		
+	}
+
+	@Test
+	void testaValorFinal()	{
+		
+	}
+
+	@Test
+	void testaValidade()	{
 		
 	}
 
