b4298b8439bec7803812c5e3a11dc3f6ef2c471f
LinusKipper <l.nicolas@edu.pucrs.br>
2023-11-18 16:27:32 -0300

    um dia vai

--- src/main/java/com/example/sistemaVendas/Aplicacao/GerarRelatorio.java
+++ src/main/java/com/example/sistemaVendas/Aplicacao/GerarRelatorio.java
@@ -1,25 +1,18 @@
 package com.example.sistemaVendas.Aplicacao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.example.sistemaVendas.Aplicacao.model.Relatorio;
 import com.example.sistemaVendas.Aplicacao.services.ServicoRelatorio;
-import com.example.sistemaVendas.Persistencias.repositories.RepClienteORM;
-import com.example.sistemaVendas.Persistencias.repositories.RepOrcamentosORM;
-import com.example.sistemaVendas.Persistencias.repositories.RepProdutosORM;
 
 public class GerarRelatorio {
-    public static void main(String[] args) {
-        RepClienteORM repCliente =  new RepClienteORM();
-        RepOrcamentosORM repOrcamentos = new RepOrcamentosORM();
-        RepProdutosORM repProdutos = new RepProdutosORM();
+    @Autowired
+    private ServicoRelatorio servicoRelatorio;
 
-        ServicoRelatorio servicoRelatorio = new ServicoRelatorio(repCliente, repOrcamentos, repProdutos);
-
-        // Geração de relatórios
-        Relatorio relatorioVendasPorProduto = servicoRelatorio.analiseVendasPorProduto();
-        Relatorio relatorioDesempenhoCliente = servicoRelatorio.analiseDesempenhoCliente();
-
-        // Exemplo de exibição dos relatórios
-        System.out.println("Relatório de Vendas por Produto:\n" + relatorioVendasPorProduto);
-        System.out.println("Relatório de Desempenho do Cliente:\n" + relatorioDesempenhoCliente);
+    public Relatorio gerarRelatorio() {
+        Relatorio r = new Relatorio();
+        r = servicoRelatorio.analiseDesempenhoCliente();
+        r = servicoRelatorio.analiseVendasPorProduto();
+        return r;
     }
 }
