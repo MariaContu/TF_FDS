17b8fe51642374f06998c98341a9cdaa9d42c385
Maria Contu <mariaescontu@gmail.com>
2023-11-18 20:01:59 +0000

    building tests

--- src/test/java/com/example/sistemaVendas/SistemaVendasApplicationTests.java
+++ src/test/java/com/example/sistemaVendas/SistemaVendasApplicationTests.java
@@ -1,10 +1,19 @@
 package com.example.sistemaVendas;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.sistemaVendas.Dominio.model.Pedido;
+import com.example.sistemaVendas.Dominio.model.ItemPedido;
+import com.example.sistemaVendas.Dominio.model.Orcamento;
 import com.example.sistemaVendas.Dominio.repositories.IRepProdutos;
 import com.example.sistemaVendas.Dominio.services.ServicoVendas;
 
@@ -24,7 +33,15 @@ class SistemaVendasApplicationTests {
 
 	@Test
 	void testaCustoDoPedido()	{
-		
+		List<ItemPedido> listaPedido = new LinkedList<>();
+		listaPedido.add(new ItemPedido(1, 10));
+		listaPedido.add(new ItemPedido(2, 15));
+		Pedido novoPedido = new Pedido(1, 2, listaPedido);
+		Orcamento novOrcamento = new Orcamento(1,new Date(),"Joaquim",novoPedido);
+		servicoVendas.calculaCustoPedido(novOrcamento);
+		double custoCalculado = novOrcamento.getCustoPedido();
+		double custoEsperado = 140.00;
+		assertEquals(custoEsperado, custoCalculado,0.01);
 	}
 
 	@Test
