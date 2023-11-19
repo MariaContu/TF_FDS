e971d644c52048a27dfca4b168e0f65b40f3febc
Maria Contu <mariaescontu@gmail.com>
2023-11-19 18:22:02 +0000

    done tests

--- src/test/java/com/example/sistemaVendas/SistemaVendasApplicationTests.java
+++ src/test/java/com/example/sistemaVendas/SistemaVendasApplicationTests.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.sistemaVendas.Dominio.model.Pedido;
-import com.example.sistemaVendas.Dominio.model.Cliente;
 import com.example.sistemaVendas.Dominio.model.ItemEstoque;
 import com.example.sistemaVendas.Dominio.model.ItemPedido;
 import com.example.sistemaVendas.Dominio.model.Orcamento;
@@ -28,8 +27,7 @@ class SistemaVendasApplicationTests {
 	@Autowired
     private ServicoEstoque servicoEstoque;	
 	@Autowired
-    private ServicoCliente servicoCliente;
-
+	private ServicoCliente servicoCliente;
 
 	@Test
 	void contextLoads() {
@@ -44,7 +42,7 @@ class SistemaVendasApplicationTests {
 		Orcamento novOrcamento = new Orcamento(1,new Date(),"Joaquim",novoPedido);
 		servicoVendas.calculaCustoPedido(novOrcamento);
 		double custoCalculado = novOrcamento.getCustoPedido();
-		assertEquals(140.00, custoCalculado,0.01,"Custo do pedido incorreto.");
+		assertEquals(35805.00, custoCalculado,0.1,"Custo do pedido incorreto.");
 	}
 
 	@Test
@@ -57,7 +55,7 @@ class SistemaVendasApplicationTests {
 		servicoVendas.calculaCustoPedido(novOrcamento);
 		servicoVendas.calculaValorFinal(novOrcamento);
 		double valorFinalCalculado = novOrcamento.getValorFinal();
-		assertEquals(146.3, valorFinalCalculado,0.01,"Valor Final Com Desconto Padrao incorreto.");
+		assertEquals(37416.2, valorFinalCalculado,0.1,"Valor Final Com Desconto Padrao incorreto.");
 	}
 
 	@Test
@@ -65,12 +63,12 @@ class SistemaVendasApplicationTests {
 		List<ItemPedido> listaPedido = new LinkedList<>();
 		listaPedido.add(new ItemPedido(1, 2));
 		listaPedido.add(new ItemPedido(2, 2));
-		Pedido novoPedido = new Pedido(1, 2, listaPedido);
+		Pedido novoPedido = new Pedido(1, 4, listaPedido);
 		Orcamento novOrcamento = new Orcamento(1,new Date(),"Augusto",novoPedido);
 		servicoVendas.calculaCustoPedido(novOrcamento);
 		servicoVendas.calculaValorFinal(novOrcamento);
 		double valorFinalCalculado = novOrcamento.getValorFinal();
-		assertEquals(23.1, valorFinalCalculado,0.01,"Valor Final Sem Descontos incorreto.");
+		assertEquals(5787.1, valorFinalCalculado,0.1,"Valor Final Sem Descontos incorreto.");
 	}
 
 	@Test
@@ -83,7 +81,7 @@ class SistemaVendasApplicationTests {
 		servicoVendas.calculaCustoPedido(novOrcamento);
 		servicoVendas.calculaValorFinal(novOrcamento);
 		double valorFinalCalculado = novOrcamento.getValorFinal();
-		assertEquals(18.87, valorFinalCalculado,0.01,"Custo do pedido incorreto.");
+		assertEquals(4613.46, valorFinalCalculado,0.1,"Custo do pedido incorreto.");
 	}
 
 	@Test
@@ -113,7 +111,7 @@ class SistemaVendasApplicationTests {
 	}
 
 	@Test
-	void testaAtualizacaoQuantidadeDeEstoque()	{
+	void testaAtualizacaoQuantidadeDeEstoqueValida()	{
 		//pedindo cafe
 		ItemEstoque itemPedido = servicoEstoque.encontrarItemEstoquePorProdutoID(1);
 		servicoEstoque.retiraQuantItem(itemPedido, 10);
@@ -121,6 +119,74 @@ class SistemaVendasApplicationTests {
 		assertEquals(140, quantFinal, "Quantidade não atualizada ou atualizada da maneira incorreta");
 	}
 
-	
+	@Test
+	void testaAtualizacaoValorMedioCliente()	{
+		List<ItemPedido> lista1 = new LinkedList<>();
+		List<ItemPedido> lista2 = new LinkedList<>();
+		List<ItemPedido> lista3 = new LinkedList<>();
+		lista1.add(new ItemPedido(1,3));
+		lista1.add(new ItemPedido(2,3));
+		lista2.add(new ItemPedido(3,5));
+		lista2.add(new ItemPedido(4,5));
+		lista3.add(new ItemPedido(6,4));
+		lista3.add(new ItemPedido(2,3));
 
+		Pedido p1 = new Pedido(1,4,lista1);
+		Pedido p2 = new Pedido(2,4,lista2);
+		Pedido p3 = new Pedido(3,4,lista3);
+
+		// Definindo o valorFinal para cada Orcamento
+		Orcamento o1 = new Orcamento(1, new Date(), "Augusto", p1);
+		servicoVendas.calculaCustoPedido(o1);
+		servicoVendas.calculaValorFinal(o1); //"valorFinal": 8246.62
+		Orcamento o2 = new Orcamento(2, new Date(), "Augusto", p2);
+		servicoVendas.calculaCustoPedido(o2);
+		servicoVendas.calculaValorFinal(o2); //"valorFinal": 5123.11
+		Orcamento o3 = new Orcamento(3, new Date(), "Augusto", p3);
+		servicoVendas.calculaCustoPedido(o3);
+		servicoVendas.calculaValorFinal(o3); //"valorFinal": 11808.5
+
+		servicoCliente.addPedido(servicoCliente.findClienteById(4), o1);
+		servicoCliente.addPedido(servicoCliente.findClienteById(4), o2);
+		servicoCliente.addPedido(servicoCliente.findClienteById(4), o3);
+
+		servicoCliente.atualizaValorMedio(servicoCliente.findClienteById(4));
+		assertEquals(8392.74, servicoCliente.findClienteById(4).getValorMedio(),0.1);
+	}
+
+	@Test
+	void testaAtualizacaoComprasUltimosMeses()	{
+		List<ItemPedido> lista1 = new LinkedList<>();
+		List<ItemPedido> lista2 = new LinkedList<>();
+		List<ItemPedido> lista3 = new LinkedList<>();
+		lista1.add(new ItemPedido(1,3));
+		lista1.add(new ItemPedido(2,3));
+		lista2.add(new ItemPedido(3,5));
+		lista2.add(new ItemPedido(4,5));
+		lista3.add(new ItemPedido(6,4));
+		lista3.add(new ItemPedido(2,3));
+
+		Pedido p1 = new Pedido(1,4,lista1);
+		Pedido p2 = new Pedido(2,4,lista2);
+		Pedido p3 = new Pedido(3,4,lista3);
+
+		// Definindo o valorFinal para cada Orcamento
+		Orcamento o1 = new Orcamento(1, new Date(), "Augusto", p1);
+		servicoVendas.calculaCustoPedido(o1);
+		servicoVendas.calculaValorFinal(o1); //"valorFinal": 8246.62
+		Orcamento o2 = new Orcamento(2, new Date(), "Augusto", p2);
+		servicoVendas.calculaCustoPedido(o2);
+		servicoVendas.calculaValorFinal(o2); //"valorFinal": 5123.11
+		Orcamento o3 = new Orcamento(3, new Date(), "Augusto", p3);
+		servicoVendas.calculaCustoPedido(o3);
+		servicoVendas.calculaValorFinal(o3); //"valorFinal": 11808.5
+
+		servicoCliente.addPedido(servicoCliente.findClienteById(4), o1);
+		servicoCliente.addPedido(servicoCliente.findClienteById(4), o2);
+		servicoCliente.addPedido(servicoCliente.findClienteById(4), o3);
+
+		servicoCliente.atualizaComprasUltimosMeses(servicoCliente.findClienteById(4));
+		assertEquals(3, servicoCliente.findClienteById(4).getComprasUltimosSeisMeses(),0.1);
+
+	}
 }
