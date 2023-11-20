9cc1366bfb4c4fdfe6988675053aada505743fdc
Maria <112641497+MariaContu@users.noreply.github.com>
2023-11-19 22:40:57 -0300

    Update Relatorio.java

--- src/main/java/com/example/sistemaVendas/Aplicacao/model/Relatorio.java
+++ src/main/java/com/example/sistemaVendas/Aplicacao/model/Relatorio.java
@@ -1,5 +1,45 @@
 package com.example.sistemaVendas.Aplicacao.model;
 
 public class Relatorio {
-    
+    private String conteudo;
+    private double custoTotalMedio;
+    private double custoMedioFinal;
+    private int mediaItensPorPedido;
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void adicionarConteudo(String informacao) {
+        conteudo += informacao + "\n";
+    }
+
+    @Override
+    public String toString() {
+        return conteudo;
+    }
+
+    public double getCustoTotalMedio() {
+        return custoTotalMedio;
+    }
+
+    public double getCustoMedioFinal() {
+        return custoMedioFinal;
+    }
+
+    public int getMediaItensPorPedido() {
+        return mediaItensPorPedido;
+    }
+
+    public void setAverageItensPerOrder(int mediaItensPorPedido) {
+        this.mediaItensPorPedido = mediaItensPorPedido;
+    }
+
+    public void setAverageTotalCost(double custoTotalMedio) {
+        this.custoTotalMedio = custoTotalMedio;
+    }
+
+    public void setAverageFinalCost(double custoMedioFinal) {
+        this.custoMedioFinal = custoMedioFinal;
+    }
 }
