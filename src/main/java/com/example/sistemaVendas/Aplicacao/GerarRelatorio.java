46d714bf821d89acfdfe94b1cc6e2332a0860fe3
Maria <112641497+MariaContu@users.noreply.github.com>
2023-11-19 22:41:34 -0300

    Update GerarRelatorio.java

--- src/main/java/com/example/sistemaVendas/Aplicacao/GerarRelatorio.java
+++ src/main/java/com/example/sistemaVendas/Aplicacao/GerarRelatorio.java
@@ -1,5 +1,60 @@
 package com.example.sistemaVendas.Aplicacao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.sistemaVendas.Aplicacao.model.Relatorio;
+import com.example.sistemaVendas.Aplicacao.services.ServicoRelatorio;
+
+@Component
 public class GerarRelatorio {
-    
+    @Autowired
+    private ServicoRelatorio servicoRelatorio;
+
+    public Relatorio gerarTodosRelatorios() {
+        Relatorio relatorio = new Relatorio();
+
+        // Adicione o relatório de desempenho do cliente
+        relatorio.adicionarConteudo("Relatório de Desempenho do Cliente:\n");
+        relatorio.adicionarConteudo(servicoRelatorio.analiseDesempenhoCliente().getConteudo());
+
+        // Adicione o relatório de vendas por produto
+        relatorio.adicionarConteudo("\nRelatório de Vendas por Produto:\n");
+        relatorio.adicionarConteudo(servicoRelatorio.analiseVendasPorProduto().getConteudo());
+
+        relatorio.adicionarConteudo("\nRelatório do Custo Medio dos Orçamantos");
+        relatorio.adicionarConteudo(servicoRelatorio.calculaCustoMedioOrcamentos());
+
+        return relatorio;
+    }
+
+    public Relatorio relatorioDesempenhoDoCliente() {
+        Relatorio relatorio = new Relatorio();
+
+        // Adicione o relatório de desempenho do cliente
+        relatorio.adicionarConteudo("Relatório de Desempenho do Cliente:\n");
+        relatorio.adicionarConteudo(servicoRelatorio.analiseDesempenhoCliente().getConteudo());
+
+        return relatorio;
+    }
+
+    public Relatorio relatorioVendasPorProduto() {
+        Relatorio relatorio = new Relatorio();
+
+        // Adicione o relatório de vendas por produto
+        relatorio.adicionarConteudo("\nRelatório de Vendas por Produto:\n");
+        relatorio.adicionarConteudo(servicoRelatorio.analiseVendasPorProduto().getConteudo());
+
+        return relatorio;
+    }
+
+    public Relatorio relatorioCustoMedioOrcamentos() {
+        Relatorio relatorio = new Relatorio();
+
+        relatorio.adicionarConteudo("\nRelatório do Custo Medio dos Orçamantos");
+        relatorio.adicionarConteudo(servicoRelatorio.calculaCustoMedioOrcamentos());
+
+        return relatorio;
+    }
+
 }
